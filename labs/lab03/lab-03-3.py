cf4c4b28e7aca14bdcb8574a6025036d74017712
sahirahbleuuughh <nursahirahribin@gmail.com>
2025-07-22 12:51:10 +0800

    update

--- labs/lab03/lab-03-3.py
+++ labs/lab03/lab-03-3.py
@@ -1,8 +1,8 @@
 kilobyte = float(input())
 megabyte = kilobyte * 0.0977
 gigabyte = kilobyte * 0.0000954
-terabyte = kilobyte * 0.0000000931
-petabyte = kilobyte * 0.0000000000000909
+terabyte = kilobyte * 0.000000000931
+petabyte = kilobyte * 0.000000000000909
 print(megabyte)
 print(gigabyte)
 print(terabyte)
