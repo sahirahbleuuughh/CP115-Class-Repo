5e5d66159dc848d8c87db7792933832e0a4ada5f
sahirahbleuuughh <nursahirahribin@gmail.com>
2025-07-22 12:25:06 +0800

    update

---
+++ labs/lab03/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte * 0.0001
+gigabyte = kilobyte * 0.00001
+terabyte = kilobyte * 0.000001
+petabyte = kilobyte * 0.0000001
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
