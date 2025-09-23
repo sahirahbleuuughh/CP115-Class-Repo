2614e853d311fd13b84af82844821e39002f3df7
sahirahbleuuughh <nursahirahribin@gmail.com>
2025-09-23 11:21:38 +0800

    ok

--- labs/lab09/exercise5/exercise5.py
+++ labs/lab09/exercise5/exercise5.py
@@ -1,3 +1,17 @@
+# Calculate restaurant bills with menu items and discounts.
+
+# Menu Pricing:
+
+#     Main Course: Chicken RM10, Beef RM12, Fish RM11
+#     Drink: Soft Drink RM2, Coffee RM3
+#     Dessert: Ice Cream RM4, Cake RM5
+
+# Additional Charges:
+
+#     Service charge: 10% on total food cost
+#     Senior citizen discount (age more than 60): 15% off final bill
+#     Student discount (age less than 18): 10% off final bill
+
 main_course = input()
 drink = input()
 dessert = input()
